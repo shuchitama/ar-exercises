4b3b6431d95d2d59fbd31c7bedf059017067eb52
Shuchita Mahey <shuchitamahey@Shuchitas-MBP.hitronhub.home>
2020-03-31 10:16:53 -0700

    load store by id, update name for one store

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -5,3 +5,12 @@ puts "Exercise 2"
 puts "----------"
 
 # Your code goes here ...
+
+@store1 = Store.find_by(id: 1)
+@store2 = Store.find_by(id: 2)
+puts @store1.id
+puts @store2.id
+
+@store1.update(name: "Delta")
+puts @store1.name
+
