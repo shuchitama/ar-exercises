a4b5410c36c1af53c9aa771e3a46b7376e399780
Shuchita Mahey <shuchitamahey@Shuchitas-MBP.hitronhub.home>
2020-03-31 10:17:30 -0700

    select stores with some conditions

--- exercises/exercise_4.rb
+++ exercises/exercise_4.rb
@@ -7,3 +7,14 @@ puts "Exercise 4"
 puts "----------"
 
 # Your code goes here ...
+
+# 4. Do another fetch but this time load stores that carry women's apparel and are generating less than $1M in annual revenue.
+surrey = Store.create(annual_revenue: 224000, name: "Surrey", mens_apparel: false, womens_apparel: true)
+whistler = Store.create(annual_revenue: 1900000, name: "Whistler", mens_apparel: true, womens_apparel: false)
+yaletown = Store.create(annual_revenue: 430000, name: "Yaletown", mens_apparel: true, womens_apparel: true)
+
+@mens_stores = Store.where(mens_apparel: true)
+@womens_stores = Store.where("womens_apparel = ? AND annual_revenue < ?", true, 1000000)
+puts "---RESULT---"
+@mens_stores.each { |store| puts "Name: #{store.name}, Annual Revenue: #{store.annual_revenue}"}
+@womens_stores.each { |store| puts "Name: #{store.name}, Annual Revenue: #{store.annual_revenue}"}
