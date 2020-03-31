57cd0982c9f3004de6fbd44e0b0613c436faef00
Shuchita Mahey <shuchitamahey@Shuchitas-MBP.hitronhub.home>
2020-03-31 10:59:58 -0700

    comment-out puts statements

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -10,6 +10,6 @@ puts "----------"
 @sum_revenue = Store.sum("annual_revenue")
 @avg_revenue = Store.average("annual_revenue")
 @rich_stores = Store.where("annual_revenue >= ?", 1000000).count
-puts "Total Revenue: #{@sum_revenue}"
-puts "Average Revenue: #{@avg_revenue}"
-puts "Num of rich stores: #{@rich_stores}"
+# puts "Total Revenue: #{@sum_revenue}"
+# puts "Average Revenue: #{@avg_revenue}"
+# puts "Num of rich stores: #{@rich_stores}"
