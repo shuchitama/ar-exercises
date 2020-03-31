9ca26e461292134136ce4301f4a8fc22373ae485
Shuchita Mahey <shuchitamahey@Shuchitas-MBP.hitronhub.home>
2020-03-31 10:28:58 -0700

    sum, average, and chained where and count

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -7,4 +7,9 @@ require_relative './exercise_4'
 puts "Exercise 5"
 puts "----------"
 
-# Your code goes here ...
+@sum_revenue = Store.sum("annual_revenue")
+@avg_revenue = Store.average("annual_revenue")
+@rich_stores = Store.where("annual_revenue >= ?", 1000000).count
+puts "Total Revenue: #{@sum_revenue}"
+puts "Average Revenue: #{@avg_revenue}"
+puts "Num of rich stores: #{@rich_stores}"
