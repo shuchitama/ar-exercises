3114e64e2282508f6002136f9541beb7f4b7db54
Shuchita Mahey <shuchitamahey@Shuchitas-MBP.hitronhub.home>
2020-03-30 21:22:00 -0700

    add three stores

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -4,3 +4,9 @@ puts "Exercise 1"
 puts "----------"
 
 # Your code goes below here ...
+
+burnaby = Store.create(annual_revenue: 300000, name: "Burnaby", mens_apparel: true, womens_apparel: true)
+richmond = Store.create(annual_revenue: 1260000, name: "Richmond", mens_apparel: false, womens_apparel: true)
+gastown = Store.create(annual_revenue: 190000, name: "Gastown", mens_apparel: true, womens_apparel: false)
+
+puts Store.count
